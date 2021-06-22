a79477e2fb07d943498aaa95eb5607fa2ed241c1
asviananda <62512432+asviananda@users.noreply.github.com>
2021-06-22 23:59:24 +0700

    done

---
+++ biodatapbkk/application/views/frontpage.php
@@ -0,0 +1,109 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<title>Proilenya Okta</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://1.bp.blogspot.com/-aq0LKOiZs6Y/Xus9LL2bySI/AAAAAAAAAP4/_NPOzaOhyywFdkIFbpfRTHVUpTO84idYACK4BGAsYHg/s320/FOTO%2B%25282%2529.JPG" style="width:100%" alt="Avatar">
+          <div class="w3-container w3-text-black">
+            <h2>Oktarizka Asviananda Nursanty</h2>
+          </div>
+        </div>
+        <div class="w3-container">   
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Informatika ITS 2018</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Cikupa, Tangerang</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>oktarizkasviananda@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>089629066641</p>
+          <hr>
+
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+      <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Institut Teknologi Sepuluh Nopember</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2018-Now</h6>
+          <p>OTW Bachelor Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>SMA Negeri 1 Tangerang</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2015 - 2018</h6>
+          <p>Highschool</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>SMPN 1 Tangerang</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2012 - 2015</h6>
+          <p>Middle School</p><br>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>SD Citra Islami</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2006 - 2012</h6>
+          <p>Elementary School</p><br>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>TK Prawiyasa</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2004 - 2006</h6>
+          <p>Kindergarten</p><br>
+        </div>
+      </div>
+      
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Facts</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>I'm a Libra Sun with Leo Moon and Pisces Ascendant.</b></h5>
+          <h5 class="w3-opacity"><b>Also, I'm an ISFP!</b></h5>
+          <h5 class="w3-opacity"><b>Tujuan hidup ini hanyalah menjadi pekerja keras yang kaya harta dunia dan kaya amalan akhirat.</b></h5>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Visit <a href="https://tugasadalahmakanankeseharianku.blogspot.com" target="_blank">my blog!^^</a></p>
+</footer>
+
+</body>
+</html>
